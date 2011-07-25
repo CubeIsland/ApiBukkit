e594701c8aed47fdf2d89a4418ebf092a0d8def3
Phillip Schichtel <quick_wango@code-infection.de>
2011-07-25 16:50:29 +0200

    clean up unused imports

--- BasicApi/src/main/java/de/codeinfection/quickwango/BasicApi/BasicApi.java
+++ BasicApi/src/main/java/de/codeinfection/quickwango/BasicApi/BasicApi.java
@@ -8,8 +8,6 @@ import org.bukkit.plugin.java.JavaPlugin;
 import de.codeinfection.quickwango.ApiBukkit.ApiBukkit;
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
-import java.util.List;
-import java.util.ArrayList;
 
 /**
  *
