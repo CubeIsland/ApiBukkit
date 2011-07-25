474624ce6b6c77576242daa64fcb0bc3e4960400
Phillip Schichtel <quick_wango@code-infection.de>
2011-07-25 16:51:05 +0200

    updated ApiCommandSender to Bukkit #1000+

--- BasicApi/src/main/java/de/codeinfection/quickwango/BasicApi/ApiCommandSender.java
+++ BasicApi/src/main/java/de/codeinfection/quickwango/BasicApi/ApiCommandSender.java
@@ -2,8 +2,13 @@ package de.codeinfection.quickwango.BasicApi;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
 import org.bukkit.Server;
 import org.bukkit.command.CommandSender;
+import org.bukkit.permissions.Permission;
+import org.bukkit.permissions.PermissionAttachment;
+import org.bukkit.permissions.PermissionAttachmentInfo;
+import org.bukkit.plugin.Plugin;
 
 /**
  *
@@ -62,4 +67,58 @@ public class ApiCommandSender implements CommandSender
     {
         return this.server;
     }
+
+    public boolean isPermissionSet(String name)
+    {
+        return true;
+    }
+
+    public boolean isPermissionSet(Permission perm)
+    {
+        return true;
+    }
+
+    public boolean hasPermission(String name)
+    {
+        return true;
+    }
+
+    public boolean hasPermission(Permission perm)
+    {
+        return true;
+    }
+
+    public PermissionAttachment addAttachment(Plugin plugin, String name, boolean value)
+    {
+        return null;
+    }
+
+    public PermissionAttachment addAttachment(Plugin plugin)
+    {
+        return null;
+    }
+
+    public PermissionAttachment addAttachment(Plugin plugin, String name, boolean value, int ticks)
+    {
+        return null;
+    }
+
+    public PermissionAttachment addAttachment(Plugin plugin, int ticks)
+    {
+        return null;
+    }
+
+    public void removeAttachment(PermissionAttachment attachment)
+    {}
+
+    public void recalculatePermissions()
+    {}
+
+    public Set<PermissionAttachmentInfo> getEffectivePermissions()
+    {
+        return null;
+    }
+
+    public void setOp(boolean value)
+    {}
 }
