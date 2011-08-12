443efc948673a72f80309128538b5ef4b2c98678
Phillip Schichtel <quick_wango@code-infection.de>
2011-08-12 19:09:13 +0200

    hopefully fixed the armor calculation

--- BasicApi/src/main/java/de/codeinfection/quickwango/BasicApi/Controller/PlayerController.java
+++ BasicApi/src/main/java/de/codeinfection/quickwango/BasicApi/Controller/PlayerController.java
@@ -73,7 +73,37 @@ public class PlayerController extends AbstractRequestController
     
     private class InfoAction extends RequestAction
     {
-        private final double armorPoints[] = {1.5, 3.0, 4.0, 1.5};
+        private final double baseArmorPoints[] = {1.5, 3.0, 4.0, 1.5};
+
+        public final int getArmorPoints(Player player)
+        {
+            int currentDurabilitySum = 0;
+            int maxDurabilitySum = 0;
+            double baseArmorPointsSum = 0;
+            ItemStack armorContents[] = player.getInventory().getArmorContents();
+            for (int i = 0; i < armorContents.length; ++i)
+            {
+                if (armorContents[i] == null)
+                {
+                    continue;
+                }
+                Material material = armorContents[i].getType();
+                if (material == null)
+                {
+                    continue;
+                }
+                final short maxDurability = material.getMaxDurability();
+                if (maxDurability < 0)
+                {
+                    continue;
+                }
+                final short durability = armorContents[i].getDurability();
+                maxDurabilitySum += maxDurability;
+                currentDurabilitySum += maxDurability - durability;
+                baseArmorPointsSum += baseArmorPoints[i];
+            }
+            return (int)(2 * Math.round(baseArmorPointsSum * currentDurabilitySum / maxDurabilitySum));
+        }
 
         @Override
         public Object run(Properties params, Server server) throws RequestException
@@ -113,36 +143,6 @@ public class PlayerController extends AbstractRequestController
             {
                 throw new RequestException("No player given!", 1);
             }
-        }
-
-        public final int getArmorPoints(Player player)
-        {
-            int currentDurability = 0;
-            int baseDurability = 0;
-            double baseArmorPoints = 0;
-            ItemStack inventory[] = player.getInventory().getArmorContents();
-            for (int i = 0; i < inventory.length; ++i)
-            {
-                if (inventory[i] == null)
-                {
-                    continue;
-                }
-                Material material = inventory[i].getType();
-                if (material == null)
-                {
-                    continue;
-                }
-                final short maxDurability = material.getMaxDurability();
-                if (maxDurability < 0)
-                {
-                    continue;
-                }
-                final short durability = inventory[i].getDurability();
-                baseDurability += maxDurability;
-                currentDurability += maxDurability - durability;
-                baseArmorPoints += armorPoints[i];
-            }
-            return (int)Math.round(2 * baseArmorPoints * currentDurability / baseDurability);
         }
     }
     
