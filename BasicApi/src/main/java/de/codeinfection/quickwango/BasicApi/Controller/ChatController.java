e1666752c162b7852bfda6df42587fc5f19db8da
Phillip Schichtel <quick_wango@code-infection.de>
2011-11-09 19:39:53 +0100

    reworked the chat controller

--- BasicApi/src/main/java/de/codeinfection/quickwango/BasicApi/Controller/ChatController.java
+++ BasicApi/src/main/java/de/codeinfection/quickwango/BasicApi/Controller/ChatController.java
@@ -4,6 +4,7 @@ import de.codeinfection.quickwango.ApiBukkit.ApiPlayer;
 import de.codeinfection.quickwango.ApiBukkit.ApiRequestAction;
 import de.codeinfection.quickwango.ApiBukkit.ApiRequestController;
 import de.codeinfection.quickwango.ApiBukkit.ApiRequestException;
+import de.codeinfection.quickwango.ApiBukkit.ApiSerializable;
 import de.codeinfection.quickwango.BasicApi.BasicApi;
 import de.codeinfection.quickwango.BasicApi.BasicApiConfiguration;
 import java.util.Properties;
@@ -25,25 +26,53 @@ import org.bukkit.permissions.Permission;
  */
 public class ChatController extends ApiRequestController
 {
-    private Queue<String[]> chatLog;
+    private Queue<ChatMessage> chatLog;
     private BasicApiConfiguration config;
 
     public ChatController(BasicApi plugin)
     {
         super(plugin, true);
         this.config = plugin.getBasicApiConfig();
-        this.chatLog = new ConcurrentLinkedQueue<String[]>();
+        this.chatLog = new ConcurrentLinkedQueue<ChatMessage>();
 
         this.plugin.getServer().getPluginManager().registerEvent(Type.PLAYER_CHAT, new ChatListener(), Priority.Monitor, this.plugin);
 
         this.setAction("get", new GetAction());
-        this.setAction("send", new SendAction());
+        this.setAction("public", new PublicAction());
+        this.setAction("private", new PrivateAction());
     }
 
     @Override
     public Object defaultAction(String action, Properties params, Server server) throws ApiRequestException
     {
         return this.getActions().keySet();
+    }
+
+    private class BasicApiPlayerChatEvent extends PlayerChatEvent
+    {
+        public BasicApiPlayerChatEvent(Player player, String message)
+        {
+            super(player, message);
+        }
+    }
+
+    private final class ChatMessage implements ApiSerializable
+    {
+        public final String author;
+        public final String message;
+        public final boolean api;
+
+        public ChatMessage(final String author, final String message, final boolean api)
+        {
+            this.author = author;
+            this.message = message;
+            this.api = api;
+        }
+
+        public Object serialize()
+        {
+            return new String[] {this.author, this.message, (this.api ? "api" : "user")};
+        }
     }
 
     private class ChatPlayer extends ApiPlayer
@@ -106,7 +135,7 @@ public class ChatController extends ApiRequestController
                 chatLog.poll();
             }
             //chatLog.add(new String[] {event.getPlayer().getName(), event.getMessage().replaceAll("\u00A7([a-f0-9])", "&$1")});
-            chatLog.add(new String[] {event.getPlayer().getName(), event.getMessage()});
+            chatLog.add(new ChatMessage(event.getPlayer().getName(), event.getMessage(), (event instanceof BasicApiPlayerChatEvent)));
         }
     }
     
@@ -119,7 +148,7 @@ public class ChatController extends ApiRequestController
         }
     }
     
-    private class SendAction extends ApiRequestAction
+    private class PublicAction extends ApiRequestAction
     {
         @Override
         public Object execute(Properties params, Server server) throws ApiRequestException
@@ -131,7 +160,7 @@ public class ChatController extends ApiRequestController
                 if (messageParam != null)
                 {
                     Player chatPlayer = new ChatPlayer(nameParam, server);
-                    PlayerChatEvent event = new PlayerChatEvent(chatPlayer, messageParam);
+                    PlayerChatEvent event = new BasicApiPlayerChatEvent(chatPlayer, messageParam);
                     server.getPluginManager().callEvent(event);
                     if (event.isCancelled())
                     {
@@ -151,6 +180,49 @@ public class ChatController extends ApiRequestController
             else
             {
                 throw new ApiRequestException("No chat name given!", 1);
+            }
+            return null;
+        }
+    }
+    
+    private class PrivateAction extends ApiRequestAction
+    {
+        @Override
+        public Object execute(Properties params, Server server) throws ApiRequestException
+        {
+            String targetParam = params.getProperty("target");
+            if (targetParam != null)
+            {
+                Player target = server.getPlayerExact(targetParam);
+                if (target != null)
+                {
+                    String nameParam = params.getProperty("name");
+                    if (nameParam != null)
+                    {
+                        String messageParam = params.getProperty("message");
+                        if (messageParam != null)
+                        {
+                            String message = String.format(config.chatFormat, nameParam, messageParam);
+                            target.sendMessage(message);
+                        }
+                        else
+                        {
+                            throw new ApiRequestException("No message given!", 4);
+                        }
+                    }
+                    else
+                    {
+                        throw new ApiRequestException("No name given!", 3);
+                    }
+                }
+                else
+                {
+                    throw new ApiRequestException("Target player not found!", 2);
+                }
+            }
+            else
+            {
+                throw new ApiRequestException("No target player given!", 1);
             }
             return null;
         }
