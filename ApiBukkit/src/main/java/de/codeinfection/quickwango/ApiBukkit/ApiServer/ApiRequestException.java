b30b70768e7d2038cc209dea1ace1e4088f302f9
Phillip Schichtel <development@code-infection.de>
2012-02-17 10:29:05 +0100

    ApiRequestException documentation

--- ApiBukkit/src/main/java/de/codeinfection/quickwango/ApiBukkit/ApiServer/ApiRequestException.java
+++ ApiBukkit/src/main/java/de/codeinfection/quickwango/ApiBukkit/ApiServer/ApiRequestException.java
@@ -1,25 +1,46 @@
 package de.codeinfection.quickwango.ApiBukkit.ApiServer;
 
 /**
+ * This exception should be used to express an error during the action execution.
+ * For example when the action expects a number as parameter, but a letter was given.
  *
- * @author CodeInfection
+ * @author Phillip Schichtel
+ * @since 1.0.0
  */
 public class ApiRequestException extends RuntimeException
 {
-    int errCode;
+    private int errCode;
 
+    /**
+     * Initializes the exception with a message and an error code
+     * The given message won't be send to the client, it's just used to inform the console
+     * 
+     * @param msg the message
+     * @param errCode the error code
+     */
     public ApiRequestException(String msg, int errCode)
     {
         super(msg);
         this.errCode = errCode;
     }
 
+    /**
+     * Initializes the exception with a message and an error code and the cause of the error
+     * The given message won't be send to the client, it's just used to inform the console
+     * 
+     * @param msg the message
+     * @param errCode the error code
+     * @param cause the cause of te error
+     */
     public ApiRequestException(String msg, int errCode, Throwable cause)
     {
         super(msg, cause);
         this.errCode = errCode;
     }
     
+    /**
+     * Returns the error code
+     */
     public int getErrCode()
     {
         return this.errCode;
