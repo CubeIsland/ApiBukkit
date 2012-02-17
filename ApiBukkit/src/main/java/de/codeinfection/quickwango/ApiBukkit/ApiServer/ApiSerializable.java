210b4b91a40f4dd8183ee7d8d1697ed7183a99ba
Phillip Schichtel <development@code-infection.de>
2012-02-17 10:44:00 +0100

    ApiSerializable documentation

--- ApiBukkit/src/main/java/de/codeinfection/quickwango/ApiBukkit/ApiServer/ApiSerializable.java
+++ ApiBukkit/src/main/java/de/codeinfection/quickwango/ApiBukkit/ApiServer/ApiSerializable.java
@@ -1,10 +1,17 @@
 package de.codeinfection.quickwango.ApiBukkit.ApiServer;
 
 /**
+ * Classes which implement this interface can be directly passed to the ApiResponse
  *
- * @author CodeInfection
+ * @author Phillip Schichtel
+ * @since 1.0.0
  */
 public interface ApiSerializable
 {
+    /**
+     * This method serialzes the object into a structure of Maps, Lists and primitive types like numbers and booleans
+     *
+     * @return the serialized object
+     */
     public Object serialize();
 }
